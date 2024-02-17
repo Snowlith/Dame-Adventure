dce32ad813aa914c6d622d5cc70404c3a1b65e8e
Snowlith <vilmtorm@gmail.com>
2024-02-16 23:03:50 -0600

    The Sex Update

--- tiled/tsx/Shadow.tsx
+++ tiled/tsx/Shadow.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Shadow" tilewidth="16" tileheight="16" tilecount="8" columns="2">
- <image source="../../sprites/shadow.png" width="32" height="64"/>
+ <image source="../graphics/shadow.png" width="32" height="64"/>
  <wangsets>
   <wangset name="Unnamed Set" type="corner" tile="-1">
    <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
