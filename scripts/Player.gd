726813b7134d8c4f751815ea2dc9f6ffa556e3ee
1nt3ger <arnavtripathy01@gmail.com>
2024-02-16 22:00:45 -0600

    playur can mov nouw

---
+++ scripts/Player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed = 75
+@export var acceleration = 7.5
+@export var friction = 1
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.lerp(input_vector * speed, acceleration * delta)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction * delta)
+	
+	
+	move_and_collide(velocity * delta)
